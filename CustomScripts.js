706e8746bd3d338d11270c1ce6bea00c63b1f552
Amande <45676497+Adrastie@users.noreply.github.com>
2024-12-21 00:13:24 +0100

    pfff

--- CustomScripts.js
+++ CustomScripts.js
@@ -1,8 +1,8 @@
-switch-title.js text/javascript
-    /// switch-title.js
-    (function() {
-        const meta = document.querySelector('meta[name="title"]');
-        if (meta?.content) {
-            document.title = meta.content;
-        }
-    })();
+/// switch-title.js
+/// alias swtie.js
+(function() {
+    const meta = document.querySelector('meta[name="title"]');
+    if (meta?.content) {
+        document.title = meta.content;
+    }
+})();
